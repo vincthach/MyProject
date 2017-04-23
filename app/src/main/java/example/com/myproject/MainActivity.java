f46374b200d9a3aaebf71ede34acd97f228c43b8
Thạch IT <thachnguyenit@Thachs-MacBook-Pro.local>
2017-04-24 01:01:35 +0700

    - Init project + Add libraries: ButterKnife, CircleImage, CircularFloatingActionMenu. + Setup styles for layout (styles.xml). + Add font: Awesome to show icon.

---
+++ app/src/main/java/example/com/myproject/MainActivity.java
@@ -0,0 +1,34 @@
+package example.com.myproject;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import jp.wasabeef.recyclerview.animators.SlideInDownAnimator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+}
